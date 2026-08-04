11b5a1a0755f4dcfff921e6c6fbaa2c4a58fd287
ha kyeong <hess93hess93@gmail.com>
2026-08-04 15:46:13 +0900

    refactor: fetch를 axios로  수정

--- js/ArticleService.js
+++ js/ArticleService.js
@@ -1,26 +1,37 @@
 "use strict";
 
-import { checkValidResponse, checkValidId } from "./checkValid.js";
+export {
+  getArticleList,
+  getArticle,
+  createArticle,
+  patchArticle,
+  deleteArticle,
+};
 
-const BASE_URL = "https://panda-market-api-crud.vercel.app/articles";
+const instance = axios.create({
+  baseURL: "https://panda-market-api-crud.vercel.app",
+});
 
-// 파라미터의 기본값을 어떻게 주는 것이 상황에 적절할까
-function getArticleList(
-  { page = 1, pageSize = 10, keyword } = { page: 1, pageSize: 10 },
-) {
-  const url = new URL(BASE_URL);
-  url.searchParams.set("page", page);
-  url.searchParams.set("pageSize", pageSize);
-  keyword && url.searchParams.set("keyword", keyword);
+function checkValidId(id) {
+  if (!id) {
+    throw new Error(`ID 번호를 입력해주세요`);
+  }
+}
 
-  return fetch(url)
+function getArticleList({ page = 1, pageSize = 5, keyword = "" } = {}) {
+  return instance
+    .get(`/articles`, { params: { page, pageSize, keyword } })
     .then((response) => {
-      checkValidResponse(response);
-
-      return response.json();
+      return response.data;
     })
     .catch((error) => {
       console.error("getArticleList 오류", error);
+
+      if (error.response) {
+        console.error(error.response.status);
+        console.error(error.response.data);
+      }
+
       throw error;
     });
 }
@@ -29,14 +40,19 @@ function getArticle(articleId) {
   // 입력값 검증 오류는 네트워크 오류와 구분하기 위해 fetch Promise 체인과 분리하여 처리합니다.
   checkValidId(articleId);
 
-  return fetch(`${BASE_URL}/${articleId}`)
+  return instance
+    .get(`/articles/${articleId}`)
     .then((response) => {
-      checkValidResponse(response);
-
-      return response.json();
+      return response.data;
     })
     .catch((error) => {
       console.error("getArticle 오류", error);
+
+      if (error.response) {
+        console.error(error.response.status);
+        console.error(error.response.data);
+      }
+
       throw error;
     });
 }
@@ -46,54 +62,46 @@ function createArticle({
   content = "내용입니다",
   image = "https://example.com/...",
 } = {}) {
-  return fetch(BASE_URL, {
-    method: "POST",
-    body: JSON.stringify({
-      title,
-      content,
-      image,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
+  return instance
+    .post("/articles", { title, content, image })
     .then((response) => {
-      checkValidResponse(response);
-
-      return response.json();
+      return response.data;
     })
     .catch((error) => {
       console.error("createArticle 오류", error);
+
+      if (error.response) {
+        console.error(error.response.status);
+        console.error(error.response.data);
+      }
+
       throw error;
     });
 }
 
-function patchArticle({
+function patchArticle(
   articleId,
-  title = "게시글 제목입니다.",
-  content = "게시글 내용입니다.",
-  image = "https://example.com/...",
-} = {}) {
+  {
+    title = "게시글 제목입니다.",
+    content = "게시글 내용입니다.",
+    image = "https://example.com/...",
+  } = {},
+) {
   checkValidId(articleId);
 
-  return fetch(`${BASE_URL}/${articleId}`, {
-    method: "PATCH",
-    body: JSON.stringify({
-      title,
-      content,
-      image,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
+  return instance
+    .patch(`/articles/${articleId}`, { title, content, image })
     .then((response) => {
-      checkValidResponse(response);
-
-      return response.json();
+      return response.data;
     })
     .catch((error) => {
       console.error("patchArticle 오류", error);
+
+      if (error.response) {
+        console.error(error.response.status);
+        console.error(error.response.data);
+      }
+
       throw error;
     });
 }
@@ -101,28 +109,19 @@ function patchArticle({
 function deleteArticle(articleId) {
   checkValidId(articleId);
 
-  return fetch(`${BASE_URL}/${articleId}`, {
-    method: "DELETE",
-  })
+  return instance
+    .delete(`/articles/${articleId}`)
     .then((response) => {
-      checkValidResponse(response);
-
-      if (response.status === 204) {
-        return;
-      }
-
-      return response.json();
+      return response.data;
     })
     .catch((error) => {
       console.error("deleteArticle 오류", error);
+
+      if (error.response) {
+        console.error(error.response.status);
+        console.error(error.response.data);
+      }
+
       throw error;
     });
 }
-
-export {
-  getArticleList,
-  getArticle,
-  createArticle,
-  patchArticle,
-  deleteArticle,
-};
