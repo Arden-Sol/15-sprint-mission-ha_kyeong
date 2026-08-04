3315bb4798a0596fcf1f2dc3a64137642f0410ae
ha kyeong <hess93hess93@gmail.com>
2026-08-04 15:53:41 +0900

    refactor: axios문법으로 바꾸면서 테스트 코드 수정

--- js/main.js
+++ js/main.js
@@ -18,8 +18,22 @@ import {
 
 const button = document.querySelector("#button");
 
+// const result1 = await getProductList({});
+// const result2 = await getProduct(4233);
+// const result3 = await createProduct();
+// const result4 = await patchProduct(4233);
+// const result5 = await deleteProduct(4233);
+
+// const result1 = await getArticleList({ keyword: "" });
+// const result2 = await getArticle(1111);
+// const result3 = await createArticle();
+// const result4 = await patchArticle(1111);
+// const result5 = await deleteArticle(1111);
+
 button.addEventListener("click", function () {
-  // API 활용하기 (목록 불러오기 등)
-  getProduct(4000);
-  console.log("입력");
+  // if (result1) console.log(result1);
+  // if (result2) console.log(result2);
+  // if (result3) console.log(result3);
+  // if (result4) console.log(result4);
+  // if (result5) console.log(result5);
 });
