ab9965706af228b95eaa219c36b5da4f413decbc
ha kyeong <hess93hess93@gmail.com>
2026-08-17 21:11:51 +0900

    feat: article API 통신 확인용 UI 추가

--- js/main.js
+++ js/main.js
@@ -96,3 +96,65 @@ productDelete.addEventListener("click", async () => {
   result.textContent = JSON.stringify(product, null, 2);
   productIdInput.value = "";
 });
+
+const articleListBtn = document.querySelector("#articleList");
+const articleIdInput = document.querySelector("#articleIdInput");
+
+const articleGet = document.querySelector("#articleGet");
+const articleDelete = document.querySelector("#articleDelete");
+const articleCreate = document.querySelector("#articleCreate");
+const articlePatch = document.querySelector("#articlePatch");
+
+const articles = document.querySelectorAll("#articleForm input");
+const articlesList = [...articles];
+
+// 상품 리스트 가져오기
+articleListBtn.addEventListener("click", async () => {
+  const article = await getArticleList();
+  result.textContent = JSON.stringify(article, null, 2);
+});
+
+// ID에 해당하는 상품 가져오기
+articleGet.addEventListener("click", async () => {
+  const article = await getArticle(articleIdInput.value);
+  result.textContent = JSON.stringify(article, null, 2);
+  articleIdInput.value = ""; // 해당 칸이 비어지는 지 확인
+});
+
+// 상품 생성
+articleCreate.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  const article = new FormData();
+
+  articlesList.forEach((input) => article.append(input.name, input.value));
+
+  const res = await createArticle(article);
+  result.textContent = JSON.stringify(res, null, 2);
+
+  articlesList.forEach((input) => (input.value = ""));
+});
+
+// 상품 수정
+articlePatch.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  const article = new FormData();
+
+  articlesList.forEach((input) => article.append(input.name, input.value));
+
+  const articleId = articleIdInput.value;
+
+  const res = await patchArticle(articleId, article);
+  result.textContent = JSON.stringify(res, null, 2);
+
+  articleIdInput.value = "";
+  articlesList.forEach((input) => (input.value = ""));
+});
+
+// 상품 삭제
+articleDelete.addEventListener("click", async () => {
+  const article = await deleteArticle(articleIdInput.value);
+  result.textContent = JSON.stringify(article, null, 2);
+  articleIdInput.value = "";
+});
