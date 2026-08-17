73540c9334edaf54549e9fa15ffb7b716040cc4f
ha kyeong <hess93hess93@gmail.com>
2026-08-17 20:48:35 +0900

    feat:product API 통신 확인용 UI 추가

--- js/main.js
+++ js/main.js
@@ -15,25 +15,84 @@ import {
   patchArticle,
   deleteArticle,
 } from "./ArticleService.js";
+// 에러 -> alert으로
+// const articleBtn = document.querySelector("#article");
+const productListBtn = document.querySelector("#productList");
+const productIdInput = document.querySelector("#productIdInput");
 
-const button = document.querySelector("#button");
+const productGet = document.querySelector("#productGet");
+const productDelete = document.querySelector("#productDelete");
+const productCreate = document.querySelector("#productCreate");
+const productPatch = document.querySelector("#productPatch");
 
-// const result1 = await getProductList({});
-// const result2 = await getProduct(4233);
-// const result3 = await createProduct();
-// const result4 = await patchProduct(4233);
-// const result5 = await deleteProduct(4233);
+const products = document.querySelectorAll("#productForm input");
+const productsList = [...products];
 
-// const result1 = await getArticleList({ keyword: "" });
-// const result2 = await getArticle(1111);
-// const result3 = await createArticle();
-// const result4 = await patchArticle(1111);
-// const result5 = await deleteArticle(1111);
+const result = document.querySelector("#result");
+const reset = document.querySelector("#reset");
 
-button.addEventListener("click", function () {
-  // if (result1) console.log(result1);
-  // if (result2) console.log(result2);
-  // if (result3) console.log(result3);
-  // if (result4) console.log(result4);
-  // if (result5) console.log(result5);
+// 초기화
+reset.addEventListener("click", () => {
+  result.textContent = "";
+});
+
+// 상품 리스트 가져오기
+productListBtn.addEventListener("click", async () => {
+  const product = await getProductList();
+  result.textContent = JSON.stringify(product, null, 2);
+});
+
+// ID에 해당하는 상품 가져오기
+productGet.addEventListener("click", async () => {
+  const product = await getProduct(productIdInput.value);
+  result.textContent = JSON.stringify(product, null, 2);
+  productIdInput.value = ""; // 해당 칸이 비어지는 지 확인
+});
+
+// 상품 생성
+productCreate.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  const product = {};
+
+  productsList.forEach((input) => {
+    product[input.name] =
+      input.name === "images" || input.name === "tags"
+        ? input.value.split(",").map((item) => item.trim())
+        : input.value;
+  });
+
+  const res = await createProduct(product);
+  result.textContent = JSON.stringify(res, null, 2);
+
+  productsList.forEach((input) => (input.value = ""));
+});
+
+// 상품 수정
+productPatch.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  const product = {};
+
+  productsList.forEach((input) => {
+    product[input.name] =
+      input.name === "images" || input.name === "tags"
+        ? input.value.split(",").map((item) => item.trim())
+        : input.value;
+  });
+
+  const productId = productIdInput.value;
+
+  const res = await patchProduct(productId, product);
+  result.textContent = JSON.stringify(res, null, 2);
+
+  productIdInput.value = "";
+  productsList.forEach((input) => (input.value = ""));
+});
+
+// 상품 삭제
+productDelete.addEventListener("click", async () => {
+  const product = await deleteProduct(productIdInput.value);
+  result.textContent = JSON.stringify(product, null, 2);
+  productIdInput.value = "";
 });
