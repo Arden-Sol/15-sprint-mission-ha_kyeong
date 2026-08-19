fd8987969c6d079dc0759ef533d076b101721ae1
ha kyeong <hess93hess93@gmail.com>
2026-08-19 18:49:51 +0900

    feat: 홈 화면 마이그레이션

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,13 @@
-import logoImage from '../../images/logo.png';
-import Nav from './Nav/Nav.jsx';
 import styles from './Header.module.css';
-function Header() {
+
+function Header({ children }) {
   return (
     <header className={styles.header}>
       <a href="/" className={styles.logo}>
-        <img src={logoImage} alt="판다 마켓 로고 이미지" />
+        <img src="/images/logo.png" alt="판다 마켓 로고 이미지" />
       </a>
 
-      <Nav className={styles.nav} />
+      {children}
       <a href="#" className={styles.logInButton}>
         로그인
       </a>
